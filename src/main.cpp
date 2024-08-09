fd2c112e3f1a87aced2e599f1590f04f5201be35
U2A5F <U2A5F@outlook.com>
2024-08-09 23:45:16 +0800

    try

--- src/main.cpp
+++ src/main.cpp
@@ -33,64 +33,85 @@ _Use_decl_annotations_
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    RedirectIOToConsole();
-
-    winrt::init_apartment();
-
-    mi_version();
-
-    int r;
-
-    ccc::WindowSystem::init();
-
-    ccc::InitParams init_params{};
-    init_params.p_vas = &ccc::app_vars();
-    ccc::InitResult init_result;
-    load_dotnet(init_params, init_result);
-    ccc::app_fn_vtb() = init_result.fn_vtb;
-
     try
     {
-        std::thread(
-            []
-            {
-                ccc::app_fn_vtb().start();
+        RedirectIOToConsole();
 
-                SDL_Event event{
-                    .quit = {
-                        .type = SDL_EVENT_QUIT,
-                        .timestamp = SDL_GetTicksNS(),
-                    }
-                };
-                SDL_PushEvent(&event);
-            }
-        ).detach();
+        winrt::init_apartment();
 
-        r = ccc::WindowSystem::main_loop();
+        mi_version();
+
+        ccc::WindowSystem::init();
+
+        ccc::InitParams init_params{};
+        init_params.p_vas = &ccc::app_vars();
+        ccc::InitResult init_result;
+        load_dotnet(init_params, init_result);
+        ccc::app_fn_vtb() = init_result.fn_vtb;
+
+        int r;
+        try
+        {
+            std::thread(
+                []
+                {
+                    ccc::app_fn_vtb().start();
+
+                    SDL_Event event{
+                        .quit = {
+                            .type = SDL_EVENT_QUIT,
+                            .timestamp = SDL_GetTicksNS(),
+                        }
+                    };
+                    SDL_PushEvent(&event);
+                }
+            ).detach();
+
+            r = ccc::WindowSystem::main_loop();
+        }
+        catch (std::exception ex)
+        {
+            ccc::logger::error(ex.what());
+            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", ex.what(), nullptr);
+            r = -1;
+        } catch (winrt::hresult_error ex)
+        {
+            ccc::logger::error(ex.message());
+            MessageBox(nullptr, ex.message().c_str(), nullptr, MB_OK);
+            r = -1;
+        } catch (...)
+        {
+            ccc::logger::error("Unknown failure occurred. Possible memory corruption");
+            SDL_ShowSimpleMessageBox(
+                SDL_MESSAGEBOX_ERROR, "Error",
+                "Unknown failure occurred. Possible memory corruption", nullptr
+            );
+            r = -1;
+        }
+
+        SDL_Quit();
+
+        ccc::app_fn_vtb().exit();
+
+        return r;
     }
     catch (std::exception ex)
     {
-        ccc::logger::error(ex.what());
+        spdlog::error(ex.what());
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", ex.what(), nullptr);
-        r = -1;
+        return -1;
     } catch (winrt::hresult_error ex)
     {
-        ccc::logger::error(ex.message());
+        spdlog::error(ex.message().c_str());
         MessageBox(nullptr, ex.message().c_str(), nullptr, MB_OK);
-        r = -1;
+        return -1;
     } catch (...)
     {
-        ccc::logger::error("Unknown failure occurred. Possible memory corruption");
+        spdlog::error("Unknown failure occurred. Possible memory corruption");
         SDL_ShowSimpleMessageBox(
             SDL_MESSAGEBOX_ERROR, "Error",
             "Unknown failure occurred. Possible memory corruption", nullptr
         );
-        r = -1;
+        return -1;
     }
-
-    SDL_Quit();
-
-    ccc::app_fn_vtb().exit();
-
-    return r;
 }
